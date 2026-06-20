6deebdd5067665c15c5db355b34fb35f2467cd32
Abhishek <abhishekkaith76@gmail.com>
2026-06-20 12:28:49 +0530

    feat(alacritty): adopt provided config as editable import

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -1,11 +1,9 @@
-{ ... }:
+{ repoDir, ... }:
 {
   programs.alacritty = {
-    enable   = true;
-    settings = {
-      font.normal.family = "Iosevka Nerd Font Mono";
-      font.size          = 12;
-      window.opacity     = 0.95;
-    };
+    enable = true;
+    # the real config is the editable repo file; alacritty imports it and
+    # live-reloads on save (live_config_reload is on by default)
+    settings.general.import = [ "${repoDir}/config/alacritty/alacritty.toml" ];
   };
 }
